42ac44e06c68dabb20b8b63f1188b20208e9756e
Mathias Kolberg <mathias.ck2000@gmail.com>
2022-01-16 16:01:06 +0100

    new images

---
+++ src/data/data.js
@@ -0,0 +1,276 @@
+import next1 from '../assets/next-1.png';
+import next2 from '../assets/next-2.png';
+import next3 from '../assets/next-3.png';
+
+import unsplash1 from '../assets/unsplash-1.png';
+import unsplash2 from '../assets/unsplash-2.png';
+import unsplash3 from '../assets/unsplash-3.png';
+
+import north1 from '../assets/north-1.png';
+import north2 from '../assets/north-2.png';
+import north3 from '../assets/north-3.png';
+
+import crwn1 from '../assets/crwn-1.png';
+import crwn2 from '../assets/crwn-2.png';
+import crwn3 from '../assets/crwn-3.png';
+
+import crypto1 from '../assets/crypto-1.png';
+import crypto2 from '../assets/crypto-2.png';
+import crypto3 from '../assets/crypto-3.png';
+
+export const skillsData = [
+  {
+    id: 1,
+    name: 'JavaScript',
+  },
+  {
+    id: 2,
+    name: 'Node.js',
+  },
+  {
+    id: 3,
+    name: 'Express.js',
+  },
+  {
+    id: 4,
+    name: 'React',
+  },
+  {
+    id: 5,
+    name: 'Next.js',
+  },
+  {
+    id: 6,
+    name: 'Gatsby',
+  },
+  {
+    id: 7,
+    name: 'Webpack',
+  },
+  {
+    id: 8,
+    name: 'HTML5',
+  },
+  {
+    id: 9,
+    name: 'jQuery',
+  },
+  {
+    id: 10,
+    name: 'CSS/sass',
+  },
+  {
+    id: 11,
+    name: 'REST',
+  },
+  {
+    id: 12,
+    name: 'Graphql',
+  },
+  {
+    id: 13,
+    name: 'MongoDB',
+  },
+  {
+    id: 14,
+    name: 'Firebase',
+  },
+  {
+    id: 15,
+    name: 'PostgreSQL',
+  },
+  {
+    id: 16,
+    name: 'MongoDB',
+  },
+  {
+    id: 17,
+    name: 'Java',
+  },
+  {
+    id: 18,
+    name: 'SpringBoot',
+  },
+];
+
+export const websitesData = [
+  {
+    title: 'Cryptoverse',
+    website: 'https://cryptoverse-frontend.vercel.app',
+    repo_name: 'cryptoverse-frontend',
+    mainImg: crypto1,
+    url: '/projects/cryptoverse',
+    images: [
+      {
+        id: 2,
+        src: crypto2,
+      },
+      {
+        id: 3,
+        src: crypto3,
+      },
+    ],
+    description: [
+      {
+        title: 'Truly A masterpiece',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+      {
+        title: 'Fresh look on a brutal sport.',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+      {
+        title: 'It’s okay lmao.',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+    ],
+    element: 'element1',
+    controls: 'controls1',
+  },
+  {
+    title: 'Next.js eccomerce store',
+    repo_name: 'next-shop',
+    website: 'https://next-shop-iota.vercel.app',
+    mainImg: next1,
+    url: '/projects/nextjs-store',
+    images: [
+      {
+        id: 2,
+        src: next2,
+      },
+      {
+        id: 3,
+        src: next3,
+      },
+    ],
+    description: [
+      {
+        title: 'Truly A masterpiece',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+      {
+        title: 'Fresh look on a brutal sport.',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+      {
+        title: 'It’s okay lmao.',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+    ],
+    element: 'element2',
+    controls: 'controls2',
+  },
+  {
+    title: 'Unsplash clone',
+    website: 'https://unsplashclone-one.vercel.app',
+    repo_name: 'unsplashclone',
+    mainImg: unsplash1,
+    url: '/projects/unsplashclone',
+    images: [
+      {
+        id: 2,
+        src: unsplash2,
+      },
+      {
+        id: 3,
+        src: unsplash3,
+      },
+    ],
+    description: [
+      {
+        title: 'Truly A masterpiece',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+      {
+        title: 'Fresh look on a brutal sport.',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+      {
+        title: 'It’s okay lmao.',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+    ],
+    element: 'element3',
+    controls: 'controls3',
+  },
+  {
+    title: 'North media landing page',
+    website: 'https://media-phi.vercel.app/',
+    mainImg: north1,
+    url: '/projects/north-media',
+    images: [
+      {
+        id: 2,
+        src: north2,
+      },
+      {
+        id: 3,
+        src: north3,
+      },
+    ],
+    description: [
+      {
+        title: 'Truly A masterpiece',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+      {
+        title: 'Fresh look on a brutal sport.',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+      {
+        title: 'It’s okay lmao.',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+    ],
+    element: 'element4',
+    controls: 'controls4',
+  },
+  {
+    title: 'Crwn apperal',
+    repo_name: 'crwn-apperal',
+    website: 'https://crwn-apperal.herokuapp.com/',
+    mainImg: crwn1,
+    url: '/projects/crwn-apperal',
+    images: [
+      {
+        id: 2,
+        src: crwn2,
+      },
+      {
+        id: 3,
+        src: crwn3,
+      },
+    ],
+    description: [
+      {
+        title: 'Truly A masterpiece',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+      {
+        title: 'Fresh look on a brutal sport.',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+      {
+        title: 'It’s okay lmao.',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+    ],
+    element: 'element5',
+    controls: 'controls5',
+  },
+];
